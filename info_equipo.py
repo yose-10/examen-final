f0f33efea80e624f68cd2d106d1a43111f20831d
yose-10 <ycondorialanes@gmail.com>
2025-12-03 17:23:05 -0400

    Delete info_equipo.py

--- info_equipo.py
+++
@@ -1,6 +0,0 @@
-nombre = input("Ingresa tu nombre: ")
-pais = input("Ingresa tu país: ")
-descripcion = input("Escribe una breve descripción: ")
-
-print(f"Hola, soy {nombre} de {pais}. {descripcion}.")
-
